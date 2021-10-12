f60c5be2da259b7233ccbf15d87f84f9f7505a7e
Renan Veronez Drechsler <renanvz@outlook.com>
2021-10-12 15:01:05 -0300

    Static Site Genderation (SSG)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from 'next';
+import { GetStaticProps } from 'next';
 import Head from 'next/head';
 import { SubscribeButton } from '../components/SubscribeButton';
 import { stripe } from '../services/stripe';
@@ -32,7 +32,7 @@ export default function Home({ product }: HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve('price_1JjTRmCseiZSBQSu0sWmS9ob', {
     expand: ['product']
   })
@@ -47,6 +47,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
       product,
-    }
-  }
+    },
+    revalidate: 60 * 60 * 24 // qnto tempo em segundos eu quero que essa página seja reconstruída
+  }             // 60 seg x 60 minutos x 24 horas  = 24 hours
 }
