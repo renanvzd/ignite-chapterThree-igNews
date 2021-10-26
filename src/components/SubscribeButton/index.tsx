1925fb49eded5aa84315f43c8c5967fce4f6fe18
Renan Veronez Drechsler <renanvz@outlook.com>
2021-10-26 10:57:43 -0300

    Gerando sessao de checkout

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,10 +1,21 @@
+import { useSession, signIn } from 'next-auth/client';
+import { stripe } from '../../services/stripe';
 import styles from './styles.module.scss';
 
 interface SubscribeButtonProps {
   priceId: string;
 }
 
-export function SubscribeButton ({ priceId }: SubscribeButtonProps) {
+export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+  const [session] = useSession();
+
+  function handleSubscribe() {
+    if (!session) {
+      signIn('github')
+      return;
+    }
+  }
+
   return (
     <button
       type="button"
