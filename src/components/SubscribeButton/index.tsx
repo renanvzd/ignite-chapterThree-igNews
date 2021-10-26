8b3c02267fe3529b2140b6d89b83e29064f8731f
Renan Veronez Drechsler <renanvz@outlook.com>
2021-10-26 12:30:59 -0300

    Redirecionando para o Stripe

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,5 +1,6 @@
 import { useSession, signIn } from 'next-auth/client';
-import { stripe } from '../../services/stripe';
+import { api } from '../../services/api';
+import { getStripeJs } from '../../services/stripe-js';
 import styles from './styles.module.scss';
 
 interface SubscribeButtonProps {
@@ -9,10 +10,22 @@ interface SubscribeButtonProps {
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
   const [session] = useSession();
 
-  function handleSubscribe() {
+  async function handleSubscribe() {
     if (!session) {
       signIn('github')
       return;
+    }
+
+    try {
+      const response = await api.post('/subscribe')
+
+      const { sessionId } = response.data
+
+      const stripe = await getStripeJs();
+
+      await stripe.redirectToCheckout({ sessionId })
+    } catch (err) {
+      alert(err.message)
     }
   }
 
@@ -20,8 +33,8 @@ export function SubscribeButton({ priceId }: SubscribeButtonProps) {
     <button
       type="button"
       className={styles.subscribeButton}
+      onClick={handleSubscribe}
     >
-
       Subscribe now
     </button>
   );
--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -25,6 +25,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     })
 
     return res.status(200).json({ sessionId: stripeCheckoutSession.id })
+
   } else {
     res.setHeader('Allow', 'POST')
     res.status(405).end('Method not allowed')
